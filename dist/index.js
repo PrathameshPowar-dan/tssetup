2ddd890cb8f80ccccce47f6159d7df22634959b1
PrathameshPowar-dan <powarprathamesh579@gmail.com>
2025-09-12 23:48:18 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+let u = { name: "Dan", email: "aawdaw" };
+u.email = "JJSTARR";
+console.log("TypeScript 5555");
+console.log(u);
+export {};
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+
+type User = {
+    name: string,
+    email: string
+}
+
+let u: User = {name:"Dan",email:"aawdaw"}
+u.email="JJSTARR"
+
+console.log("TypeScript 5555")
+console.log(u);
