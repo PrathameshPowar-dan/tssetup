a5e6317d2b365842ddc76605a54e2ca5d8f70351
PrathameshPowar-dan <powarprathamesh579@gmail.com>
2025-09-30 19:52:58 +0530

    P

--- Arrays&Objects.js
+++ Arrays&Objects.js
@@ -11,4 +11,34 @@ function Large(p) {
     return max;
 }
 
-console.log(Large(a));
+// console.log(Large(a));
+
+
+// Q2. Remove duplicates from an array
+const arr = [3,1,2,3,3]
+
+function CheckDupe(p) {
+    const New = [];
+    for (let i = 0; i < p.length; i++) {
+        if (New.includes(p[i])) {
+            continue;
+        } else{
+            New.push(p[i])
+        }
+    }
+    return New;
+}
+
+console.log(CheckDupe(arr))
+
+// OR
+
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(removeDuplicates(arr));
+
+
+// Q3. Flatten a nested array
+
